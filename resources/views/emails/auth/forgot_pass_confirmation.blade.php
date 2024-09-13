33e5eff5b4d6df4e226b8fdf45cda13c9f6549c9
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-09-13 01:40:01 -0400

    fix

--- resources/views/emails/auth/forgot_pass_confirmation.blade.php
+++ resources/views/emails/auth/forgot_pass_confirmation.blade.php
@@ -10,22 +10,22 @@
     <tr>
         <td align="center" style="margin:0;padding-top:15px;padding-left:20px;padding-right:20px;padding-bottom:30px ;border-bottom: 1px solid #D9EEF2;">
             <p style="margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:36px;color:#0a1b33;font-size:24px">
-                <strong>{{ $data['title'] }}</strong>
+                <strong>{{ $title }}</strong>
             </p>
         </td>
     </tr>
     <tr>
         <td align="center" style="padding:0;margin:0;padding-bottom:15px;padding-top:30px;">
             <p style="margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:arial, 'helvetica neue', helvetica, sans-serif;line-height:19px;color:#0a1b33;font-size:16px">
-                {{ $data['text'] }}
+                {{ $text }}
             </p>
         </td>
     </tr>
     <tr>
         <td align="center" style="padding:0;margin:0;padding-bottom:40px;padding-left:40px;padding-right:40px">
             <span class="es-button-border" style="border-style:solid;border-color:#E6641C;background:#E6641C;border-width:0px 0px 2px 0px;display:block;border-radius:32px;width:auto;border-bottom-width:0px">
-                <a href="{{ $data['buttonLink'] }}" class="es-button" target="_blank" style="mso-style-priority:100 !important;text-decoration:none;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;color:#FFFFFF;font-size:16px;padding:10px 20px 10px 20px;display:block;background:#E6641C;border-radius:32px;font-family:arial, 'helvetica neue', helvetica, sans-serif;font-weight:normal;font-style:normal;line-height:19px;width:auto;text-align:center;mso-padding-alt:0;mso-border-alt:10px solid #E6641C;padding-left:5px;padding-right:5px">
-                    {{ $data['buttonText'] }}
+                <a href="{{ $buttonLink }}" class="es-button" target="_blank" style="mso-style-priority:100 !important;text-decoration:none;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;color:#FFFFFF;font-size:16px;padding:10px 20px 10px 20px;display:block;background:#E6641C;border-radius:32px;font-family:arial, 'helvetica neue', helvetica, sans-serif;font-weight:normal;font-style:normal;line-height:19px;width:auto;text-align:center;mso-padding-alt:0;mso-border-alt:10px solid #E6641C;padding-left:5px;padding-right:5px">
+                    {{ $buttonText }}
                 </a>
             </span>
         </td>
