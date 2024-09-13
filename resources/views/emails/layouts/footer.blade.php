93d4ae6093560750e06415095d4623100f004bb9
SteffaniCastro <steffani.castro.useche@gmail.com>
2024-09-13 00:57:51 -0400

    fix

--- resources/views/emails/layouts/footer.blade.php
+++ resources/views/emails/layouts/footer.blade.php
@@ -1,10 +1,10 @@
 <table cellpadding="0" cellspacing="0" class="es-left" align="left" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:left">
     <tr>
-        <td class="es-m-p20b" align="left" style="padding:0;Margin:0;width:246px">
+        <td class="es-m-p20b" align="left" style="padding:0;margin:0;width:246px">
             <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                 <tr>
                     <td align="left" style="padding:0;margin:0;padding-top:15px;font-size:0;justify-content:start;display:flex;">
-                        <table cellpadding="0" cellspacing="0" class="es-table-not-adapt es-social" dir="ltr" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
+                        <table cellpadding="0" cellspacing="0" class="es-table-not-adapt es-social tm-footer-social" dir="ltr" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                             <tr>
                                 <td colspan="5" style="padding:0;margin:0">
                                     <span style="margin:0;-webkit-text-size-adjust:none;-ms-text-size-adjust:none;mso-line-height-rule:exactly;font-family:Inter;line-height:24px;color:#FFFFFF;font-size:16px">
@@ -15,27 +15,27 @@
                             <tr>
                                 <td align="center" valign="top" width="null" style="padding:0;margin:0;">
                                     <a href="https://www.facebook.com/CumbrePetroleoyGas" target="_blank">
-                                        <img src="{{ asset('/images/facebook.svg') }}" alt="Facebook" title="Facebook" width="31" height="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic">
+                                        <i class="zmdi zmdi-facebook"></i>
                                     </a>
                                 </td>
                                 <td align="center" valign="top" width="null" style="padding:0;margin:0;">
                                     <a href="https://x.com/PetroleoyGasCo" target="_blank">
-                                        <img src="{{ asset('/images/subtract.svg') }}" alt="Subtract" title="Subtract" width="31" height="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic">
+                                        <i class="zmdi zmdi-twitter"></i>
                                     </a>
                                 </td>
                                 <td align="center" valign="top" width="null" style="padding:0;margin:0;">
                                     <a href="https://www.youtube.com/@cumbredelpetroleoygas7632" target="_blank">
-                                        <img src="{{ asset('/images/youtube.svg') }}" alt="Youtube" title="Youtube" width="31" height="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic">
+                                        <i class="zmdi zmdi-youtube-play"></i>
                                     </a>
                                 </td>
                                 <td align="center" valign="top" width="null" style="padding:0;margin:0;">
                                     <a href="https://www.linkedin.com/company/cumbrepetroleoygas/" target="_blank">
-                                        <img src="{{ asset('/images/linkedin.svg') }}" alt="Linkedin" title="Linkedin" width="31" height="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic">
+                                        <i class="zmdi zmdi-linkedin"></i>
                                     </a>
                                 </td>
                                 <td align="center" valign="top" width="null" style="padding:0;margin:0;">
                                     <a href="https://www.instagram.com/cumbredelpetroleoygas/" target="_blank">
-                                        <img src="{{ asset('/images/instagram.svg') }}" alt="Instagram" title="Instagram" width="31" height="32" style="display:block;border:0;outline:none;text-decoration:none;-ms-interpolation-mode:bicubic">
+                                        <i class="zmdi zmdi-instagram"></i>
                                     </a>
                                 </td>
                             </tr>
@@ -48,10 +48,10 @@
 </table>
 <table cellpadding="0" cellspacing="0" class="es-right" align="right" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px;float:right">
     <tr>
-        <td align="left" style="padding:0;Margin:0;width:254px">
+        <td align="left" style="padding:0;margin:0;width:254px">
             <table cellpadding="0" cellspacing="0" width="100%" style="mso-table-lspace:0pt;mso-table-rspace:0pt;border-collapse:collapse;border-spacing:0px">
                 <tr>
-                    <td align="right" style="padding:0;Margin:0;font-size:0px">
+                    <td align="right" style="padding:0;margin:0;font-size:0px">
                         <img src="{{ asset('/images/peoples.png') }}" width="200px">
                     </td>
                 </tr>
